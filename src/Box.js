559d106fc31dbacc8a284d1fc0d8f9c4b1b871fe
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-07 11:48:14 +0300

    created handleClick function

--- src/Box.js
+++ src/Box.js
@@ -5,17 +5,22 @@ import './Box.css'
 
 class Box extends Component {
     static defaultProps = {
-        allColors: ['purple', 'magenta', 'lilac', 'pink'] 
+        allColors: ['purple', 'magenta', 'violet', 'pink'] 
     }
 
     constructor(props){
         super(props)
-        this.state = {color: choice}
+        this.state = {color: choice(this.props.allColors)}
         this.handleClick = this.handleClick.bind(this)
     }
 
-    handleClick(){
+    pickColor(){
+        let newColor = choice(this.props.allColors)
+        this.setState({ color: newColor})
+    }
 
+    handleClick(){
+        this.pickColor()
     }
 
     render() {
