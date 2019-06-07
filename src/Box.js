324d2410f5dc7c38ffde25e5ab1d309f11188523
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-07 11:39:25 +0300

    applied css style for box component

--- src/Box.js
+++ src/Box.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import 'Box.css'
+import './Box.css'
 class Box extends Component {
     constructor(props){
         super(props)
@@ -13,7 +13,7 @@ class Box extends Component {
 
     render() {
         return(
-            <div className="Box" style={{backgroundColor: this.state.color} onClick={this.handleClick}}>
+            <div className="Box" style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
 
             </div>
         )
