340e588da018408ecc1f34dbde37e27820fb18b7
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-07 12:34:11 +0300

    completed the exercise

--- src/Box.js
+++ src/Box.js
@@ -4,13 +4,11 @@ import './Box.css'
 
 
 class Box extends Component {
-    static defaultProps = {
-        allColors: ['purple', 'magenta', 'violet', 'pink'] 
-    }
+    
 
     constructor(props){
         super(props)
-        this.state = {color: choice(this.props.allColors)}
+        this.state = {color: choice(this.props.colors)}
         this.handleClick = this.handleClick.bind(this)
     }
 
@@ -18,7 +16,7 @@ class Box extends Component {
         let newColor
         
         do {
-            newColor = choice(this.props.allColors)
+            newColor = choice(this.props.colors)
         } while (newColor === this.state.color)
         
         this.setState({ color: newColor})
